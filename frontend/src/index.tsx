db4915ce89d7ddce36728108334346a39ca743ce
Chris Westbrook <westbrook.chris@gmail.com>
2024-05-04 18:11:59 +0000

    add app insights to react code

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,18 @@
 import React from "react";
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { ReactPlugin} from '@microsoft/applicationinsights-react-js';
+
+var reactPlugin = new ReactPlugin();
+var appInsights = new ApplicationInsights({
+    config: {
+        connectionString: 'InstrumentationKey=26c75ae5-db2b-45dc-9410-e7b924772b48;IngestionEndpoint=https://eastus2-4.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/;ApplicationId=a914badb-2080-4348-b137-a2a7df54b569',
+        enableAutoRouteTracking: true,
+        extensions: [reactPlugin]        
+    }
+});
+
+appInsights.loadAppInsights();
+
 import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { initializeIcons } from "@fluentui/react";
